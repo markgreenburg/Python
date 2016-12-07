217d0f9a98f5e48e2ba9d98d02ccf3edc13f148d
markgreenburg <marek.greenburg@gmail.com>
2016-12-07 10:53:02 -0600

    Added method calls to script

--- string_exercises.py
+++ string_exercises.py
@@ -6,58 +6,38 @@ def reverse_string(string):
     reversed_string = ""
     for i in range(1, len(string) + 1):
         reversed_string += string[len(string) - i]
-    print reversed_string
+    print "reversed string: %s" % reversed_string
 
 # 2.1) Ceasar Cypher
-# This method is expensive, so commented out in favor of caesar_cypher_faster
-# def caesar_encrypt(string, offset):
-#     '''Encrypts the input string by offsetting by the offset amount'''
-#     plaintext = "abcdefghijklmnopqrstuvwxyz"
-#     encrypted = plaintext[offset:] + plaintext[:offset]
-#     encryption_key = {}
-    #here we're using a range to iterate through. Should be using object
-    # iteration instead
-    # for i in range(len(plaintext) - 1):
-    #     encryption_key.update({plaintext[i]: encrypted[i]})
-    # print encryption_key
-    # plaintext_string = string.lower()
-    # encrypted_string = ""
-    # for j in range(len(plaintext_string)):
-    #     if encryption_key.get(plaintext_string[j]):
-    #         encrypted_string += encryption_key[plaintext_string[j]]
-    #     else:
-    #         encrypted_string += plaintext_string[j]
-    # print encrypted_string
-
-# 2.2) Ceasar Cypher
-# This method is better...
+# Encrypt a message with using the Caesar Cypher
 def caesar_encrypt_faster(string, offset):
     '''Encrypts the input string by offsetting by the offset amount'''
-    string = string.lower()
-    # define some special characters to pass through without modifying
-    special_chars = " ~!@#$%^&*()-+_=[]|:;"'<>,.?/'",.0123456789"
-    plaintext_key = "abcdefghijklmnopqrstuvwxyz"
-    encrypted_key = plaintext_key[offset:] + plaintext_key[:offset] + special_chars
-    plaintext_key += special_chars
+    string = string.upper()
+    plaintext_key = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    encrypted_key = plaintext_key[offset:] + plaintext_key[:offset]
     encrypted_string = ''
     for char in string:
-        encrypted_string += encrypted_key[plaintext_key.index(char)]
-    print encrypted_string
+        if char.isalpha():
+            encrypted_string += encrypted_key[plaintext_key.index(char)]
+        else:
+            encrypted_string += char
+    print "encrypted string: %s" % encrypted_string
 
 def leetspeak(string):
     '''Replaces certain alpha characters with their leetspeak num equivalents.
     Prints translated string and outputs nothing.'''
     # treat all characters as uppercase
     string = string.upper()
-    # define some special characters to pass through without modifying
-    special_chars = " ~!@#$%^&*()-+_=[]|:;"'<>,.?/'",.0123456789"
     plaintext_key = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     leetspeak_key = '4BCD3F6H1JKLMN0PQR57UVWXYZ'
-    # add common non-alphanum symbols to both strings. Alt. would be to loop
-    # through and only replace chars if they're in the list.
-    plaintext_key += special_chars
-    leetspeak_key += special_chars
     translated_text = ''
     for char in string:
-        translated_text += leetspeak_key[plaintext_key.index(char)]
-    print translated_text
+        if char.isalpha():
+            translated_text += leetspeak_key[plaintext_key.index(char)]
+        else:
+            translated_text += char
+    print "leetspeak text: %s" % translated_text
+
+reverse_string("some such 1234 $#@ nonsense")
+caesar_encrypt_faster("some such 1234 $#@ nonsense",13)
+leetspeak("some such 1234 $#@ nonsense")
