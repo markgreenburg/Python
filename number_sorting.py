202943aadc8b22e7c62ccee206ecdfe65b434c71
markgreenburg <marek.greenburg@gmail.com>
2016-12-11 22:57:11 -0600

    Optimized bubble sort
    Optimized to ensure values that have already been sorted to final
    position aren’t iterated anymore in bubble search.

--- number_sorting.py
+++ number_sorting.py
@@ -35,7 +35,7 @@ def selection_sort(random_list):
     for index_value in range(len(random_list)):
         new_min = find_min_index(random_list, index_value)
         swap_values(random_list, index_value, new_min)
-    print random_list
+    return random_list
 
 # Bubble sort
 def bubble_sort(random_list):
@@ -43,36 +43,50 @@ def bubble_sort(random_list):
     Uses bubble sort algorithm to sort a randomized list taken in as an arg.
     Returns sorted list.
     '''
+    iteration_range = len(random_list) - 1
     swap = True
     while swap:
         swap = False
-        for i in range(1, len(random_list) - 1):
+        for i in range(1, iteration_range):
             if random_list[i - 1] > random_list[i]:
                 random_list[i - 1], random_list[i] = random_list[i],\
                 random_list[i - 1]
                 swap = True
+        # We know that each pass sorts at least the last item in the list into
+        # correct position
+        iteration_range -= 1
     return random_list
 
 def test():
     '''
     Test the selection sort code with random input
     '''
-    user_list = [4, 3, 1, 8, 3, 1, 2, 9]
-    print bubble_sort(user_list)
-    # user_list = range(10000)
+    # user_list = [4, 3, 1, 8, 3, 1, 2, 9]
+    user_list = range(1000)
+    random.shuffle(user_list)
+    copy_1 = user_list
+    copy_2 = user_list
+    copy_3 = user_list
+    start_time = time.time()
+    bubble_sort(copy_1)
+    print "Normal bsort took %s seconds" % (time.time() - start_time)
+    start_time = time.time()
+    bubble_sort(copy_2)
+    print "Optimized bsort took %s seconds" % (time.time() - start_time)
+    start_time = time.time()
+    selection_sort(copy_3)
+    print "Selection sort took %s seconds" % (time.time() - start_time)
     # random.shuffle(user_list)
     # # Copy same shuffled list for other sort comparisons
     # ul2 = user_list
     # # Timed execution of our selection sort
-    # start_time = time.time()
-    # selection_sort(user_list)
-    # print "Selection sort took %s seconds" % (time.time() - start_time)
+
     # # Compare our results to the internal sort function
     # start_time = time.time()
     # ul2.sort()
     # print "Python's sort function took %s seconds" % (time.time() - start_time)
     # # No surprise...Python's
 
-#
+
 if __name__ == "__main__":
     test()
